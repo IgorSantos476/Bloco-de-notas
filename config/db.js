d0fc7e53d22a7e80390559c4dbdc47fbf7269a89
Igor Santos <figor3129@gmail.com>
2022-05-11 20:54:30 -0300

    OK

--- config/db.js
+++
@@ -1,7 +0,0 @@
-if(process.env.NODE_ENV == "production") {
-	module.exports = {
-		mongoURI: "mongodb+srv://IgorFreitas:xpcsms02@cluster0.aal5q.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-	}
-} else {
-	module.exports = {mongoURI: "mongodb://localhost/blocoNotas"}
-}
